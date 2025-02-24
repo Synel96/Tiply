9dfcc9741ca6a0a906bb1f792968035e127e26bd
Szilveszter Nemeth <nmszilveszter@gmail.com>
2025-02-24 12:26:06 +0100

    new functions and design
    added event listener(pressing enter call the function with 15% , after calling the functuion the inputs set to default , currency option and validation.

--- script.js
+++ script.js
@@ -3,9 +3,16 @@
 function calcTip(percentage) {
   let bill = parseFloat(document.getElementById("bill").value);
   let name = document.getElementById("name").value;
+  let currency = document.getElementById("currency").value;
   if (isNaN(bill) || bill <= 0) {
     document.getElementById("result").innerHTML =
       "Please enter a valid bill amount.";
+    return;
+  }
+  if (currency.trim() === "") {
+    document.getElementById("result").innerHTML = alert(
+      "Please select a currency."
+    );
     return;
   }
 
@@ -15,5 +22,15 @@ function calcTip(percentage) {
     "result"
   ).innerHTML = `Hi ${name}!The amount of your chosen tip is: ${tip.toFixed(
     2
-  )}!The total amount of your bill is :${total}`;
+  )} ${currency.toUpperCase()}!The total amount of your bill is : ${total} ${currency.toUpperCase()}!`;
+
+  document.getElementById("name").value = "";
+  document.getElementById("bill").value = "";
+  document.getElementById("currency").value = "";
 }
+
+addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    calcTip(0.15);
+  }
+});
